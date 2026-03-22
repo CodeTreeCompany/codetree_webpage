ebccc65f97694e2c8fc843580427cd4530088924
elmerurbina <[elmerurbina570@gmail.com]>
2026-03-22 06:42:09 -0600

    theme

---
+++ src/styles/index.js
@@ -0,0 +1,9 @@
+// Import all CSS files in the correct order
+import './variables.css';
+import './themes.css';
+import './base.css';
+import './construction.css';
+import './animations.css';
+import './responsive.css';
+
+// This file serves as the single entry point for all styles
